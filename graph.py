f6ed60b086a5aef01cd427ad79d605b9cb638199
Greg Olmschenk <golmschenk@gmail.com>
2014-09-10 15:04:38 -0400

    Add function to add edges and set all vertices to unvisited

--- graph.py
+++ graph.py
@@ -6,9 +6,19 @@ class Graph():
         self.number_of_vertices = 0
         self.number_of_edges = 0
         self.head_nodes = []
+        self.visited = []
 
     def initialize_with_size(self, size):
         self.number_of_vertices = size
         self.head_nodes = []
         for i in range(size):
             self.head_nodes.append([])
+
+    def add_edge(self, vertex1, vertex2):
+        self.head_nodes[vertex1].append(vertex2)
+        self.head_nodes[vertex2].append(vertex1)
+
+    def set_all_vertices_unvisited(self):
+        self.visited = []
+        for i in range(self.number_of_vertices):
+            self.visited.append(False)
