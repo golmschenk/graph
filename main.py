019408de95ee17368799b89d562b2c672e2741b7
Greg Olmschenk <golmschenk@gmail.com>
2014-10-31 15:34:19 -0400

    Rename the CSV files to have extensions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from graph import Graph
 
-g = Graph.create_weighted_graph_from_csv("examplegraphs/basic_weighted_graph")
+g = Graph.create_weighted_graph_from_csv("examplegraphs/basic_weighted_graph.csv")
 g.vertex_list[1].value = 0
 g.dijkstra_algorithm(1)
 print('Complete')
